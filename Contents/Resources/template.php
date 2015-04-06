0273eecd1642d47d25e780d304a98018a0cb9d60
iSkore Development Inc <development@dreamthegame.com>
2015-04-06 15:30:02 -0400

    Comment code and add information file

--- Contents/Resources/template.php
+++ Contents/Resources/template.php
@@ -1,8 +1,18 @@
+<!-- Create header for display in editor -->
 %[if edit]% <h1 class="headCode">CODE : </h1> %[endif]%
+
+<!-- Create collapsable div for organization in editor -->
 %[if edit]% <div class="colA"> %[endif]%
+
+<!-- Start PHP -->
 <?php
-
+    
+    // encase user's PHP, default for RapidWeaver Stacks is to use the %html% command
     %html=" "%
-
+    
 ?>
+<!-- End PHP -->
+
+<!-- Close collapsable div for organization in editor -->
 %[if edit]% </div> %[endif]%
+<!-- See --><a>https://yourhead.tenderapp.com/kb/stacks-api/</a><!-- for more information about developing stacks for RapidWeaver -->
---
+++ Contents/info.php
@@ -0,0 +1,17 @@
+<?php
+    
+    // STACKS API INFORMATION
+    
+    // Information for PLIST : https://yourhead.tenderapp.com/kb/stacks-api/infoplist
+    
+    // Information for PLIST ASSETS : https://yourhead.tenderapp.com/kb/stacks-api/infoplist-assets
+    
+    // Information for PLIST CUSTOM UI : https://yourhead.tenderapp.com/kb/stacks-api/infoplist-custom-user-interface
+    
+    // Information for TEMPLATES : https://yourhead.tenderapp.com/kb/stacks-api/templates
+    
+    // Information for STACKS API : https://yourhead.tenderapp.com/kb/stacks-api/stacks-api
+    
+    // Information for TEMPLATE STACKS : https://yourhead.tenderapp.com/kb/stacks-api/template-stacks
+
+?>
